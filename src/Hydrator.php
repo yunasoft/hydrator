b95b1deac3d3e31d1a8ca20fb02b3281ef097551
saint <saint@saints-Mac-mini.local>
2020-05-12 11:40:38 +0300

    Initial commit

--- src/Hydrator.php
+++ src/Hydrator.php
@@ -4,128 +4,16 @@ namespace YunaSoft\Hydrator;
 
 class Hydrator
 {
-    private array $data;
+    private $object;
     private array $map;
-    private array $reflectionClassMap = [];
 
-    /**
-     * Hydrator constructor.
-     * @param Object|array $data
-     */
-    public function __construct($data)
+    public function __construct($object)
     {
-        if (is_array($data)) {
-            $this->data = $data;
+        if (!isset($this->map)) {
+            $this->createMap($object);
         }
 
-        if (is_object($data)) {
-            $this->data = $this->extractProperties($data);
-        }
-
-        $this->map = array_keys($data);
-    }
-
-
-    private function extractProperties(Object $object): array
-    {
-        $reflection = $this->getReflectionClass($object);
-        $data = [];
-        foreach ($reflection->getProperties() as $property) {
-            $property->setAccessible(true);
-            $data[$property->name] = $property->getValue($object);
-        }
-
-        return $data;
-    }
-
-    public function map(array $map): Hydrator
-    {
-        foreach ($map as $dataKey => $propertyName) {
-            $this->map[$dataKey] = $propertyName;
-        }
-
-        return $this;
-    }
-
-    /**
-     * Creates an instance of a class filled with data according to map
-     *
-     * @param array $data
-     * @param string $className
-     * @return object
-     * @throws \ReflectionException
-     */
-    public function hydrate($data, $className): Object
-    {
-        $reflection = $this->getReflectionClass($className);
-        $object = $reflection->newInstanceWithoutConstructor();
-
-        foreach ($this->map as $dataKey => $propertyName) {
-            if (!$reflection->hasProperty($propertyName)) {
-                throw new \InvalidArgumentException("There's no $propertyName property in $className.");
-            }
-
-            if (isset($data[$dataKey])) {
-                $property = $reflection->getProperty($propertyName);
-                $property->setAccessible(true);
-                $property->setValue($object, $data[$dataKey]);
-            }
-        }
-
-        return $object;
-    }
-
-    /**
-     * Fills an object passed with data according to map
-     *
-     * @param array $data
-     * @param object $object
-     * @return object
-     * @throws \ReflectionException
-     */
-    public function hydrateInto($data, $object): Object
-    {
-        $className = get_class($object);
-        $reflection = $this->getReflectionClass($className);
-
-        foreach ($this->map as $dataKey => $propertyName) {
-            if (!$reflection->hasProperty($propertyName)) {
-                throw new \InvalidArgumentException("There's no $propertyName property in $className.");
-            }
-
-            if (isset($data[$dataKey])) {
-                $property = $reflection->getProperty($propertyName);
-                $property->setAccessible(true);
-                $property->setValue($object, $data[$dataKey]);
-            }
-        }
-
-        return $object;
-    }
-
-    /**
-     * Extracts data from an object according to map
-     *
-     * @param object $object
-     * @return array
-     * @throws \ReflectionException
-     */
-    public function extract($object): array
-    {
-        $data = [];
-
-        $className = get_class($object);
-        $reflection = $this->getReflectionClass($className);
-
-        foreach ($this->map as $dataKey => $propertyName) {
-            if ($reflection->hasProperty($propertyName)) {
-                $property = $reflection->getProperty($propertyName);
-                $property->setAccessible(true);
-                $data[$dataKey] = $property->getValue($object);
-            }
-        }
-
-        return $data;
+        $this->object = $object;
     }
 
     /**
@@ -140,6 +28,127 @@ class Hydrator
         if (!isset($this->reflectionClassMap[$className])) {
             $this->reflectionClassMap[$className] = new \ReflectionClass($className);
         }
+
         return $this->reflectionClassMap[$className];
+    }
+
+    protected function getReflection(Object $object)
+    {
+        $className = get_class($object);
+        return $this->getReflectionClass($className);
+    }
+
+    public function map(?array $map, $replace = false): Hydrator
+    {
+        if (!isset($map)) {
+            return $this;
+        }
+
+        if (!$this->is_array($map)) {
+            throw new \Exception('Map must be array');
+        }
+
+        if ($replace) {
+            $this->map = $map;
+        } else {
+            foreach ($map as $dataKey => $propertyName) {
+                $this->map[$dataKey] = $propertyName;
+            }
+        }
+
+        return $this;
+    }
+
+    private function is_array($object)
+    {
+        if (is_object($object)) {
+            return false;
+        }
+
+        if (is_array($object)) {
+            return true;
+        }
+
+        throw new \Exception('Incorrect object type');
+    }
+
+    private function objectPropertyMap(Object $object)
+    {
+        $reflection = $this->getReflection($object);
+
+        $map = [];
+
+        foreach ($reflection->getProperties() as $property) {
+            $map[] = $property->name;
+        }
+
+        return $map;
+    }
+
+    public function extract($strict = false): array
+    {
+        if (!$is_array = $this->is_array($this->object)) {
+            $reflection = $this->getReflection($this->object);
+        }
+
+        $data = [];
+        foreach ($this->map as $dataKey => $propertyName) {
+            if ($is_array) {
+                $data[$dataKey] = $this->object[$dataKey];
+            } else {
+                if ($reflection->hasProperty($propertyName)) {
+                    $property = $reflection->getProperty($propertyName);
+                    $property->setAccessible(true);
+                    $data[$dataKey] = $property->getValue($this->object);
+                } elseif ($strict) {
+                    throw new \InvalidArgumentException("There's no $propertyName property in $className.");
+                }
+            }
+        }
+
+        return $data;
+    }
+
+    public function hydrate(Object $object, $strict = false)
+    {
+        if (!$is_array = $this->is_array($this->object)) {
+            $data = (new self($this->object))->extract($strict);
+        } else {
+            $data = $this->object;
+        }
+
+        $className = get_class($object);
+        $reflection = $this->getReflectionClass($className);
+
+        foreach ($this->map as $dataKey => $propertyName) {
+            if (!$reflection->hasProperty($propertyName)) {
+                if ($strict) {
+                    throw new \InvalidArgumentException("There's no $propertyName property in $className.");
+                } else {
+                    continue;
+                }
+            }
+
+            if (isset($data[$dataKey])) {
+                $property = $reflection->getProperty($propertyName);
+                $property->setAccessible(true);
+                $property->setValue($object, $data[$dataKey]);
+            }
+        }
+
+        return $object;
+    }
+
+    private function createMap($object)
+    {
+        if ($this->is_array($object)) {
+            $properties = array_keys($object);
+        } else {
+            $properties = $this->objectPropertyMap($object);
+        }
+
+        foreach ($properties as $property) {
+            $this->map[$property] = $property;
+        }
     }
 }
